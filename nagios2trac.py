f075897db3c0f9a79bf1f9a71a14386b0df6fcea
Ingo Oeser <ingo@jimdo.com>
2013-04-19 16:34:55 +0200

    use """string""" syntax from python for long string

--- nagios2trac.py
+++ nagios2trac.py
@@ -53,36 +53,38 @@ if options.listmethods:
 #######
 summary_template = "[" + options.critical_host + "] " + options.service_state + ": " + options.description
 comment_template = "{{{ \n[" + options.critical_host + "] " + options.service_state + ": " + options.description + "\n" + options.long_output + "\n}}}"
-description_template = '=== Incident === \n \
- * Does it affect only one user/colleague? Not an incident, normal support case. \n \
- * What has been noticed? (e.g. nagios check + host that failed)\n' + comment_template + '\n \
- * Who is affected? (all users, limited set of users, departments, partners, ...) \n \
- * When did it start? (e.g. nagios reported time) \n \
- * How did you notice it (Monitoring, Support..?) \n \
- \n \
-=== Spread the word first, then act === \n \
- * Always put a link to this ticket everywhere you inform your colleagues about it \n \
- * Tell Yammer group "System Status" when the incident started (latest 30min after incident notice!) \n \
- * Update Yammer at least every hour for incidents during office hours, even if nothing changed. \n \
- * Be available in Infrastruktur channel and communicate to your colleagues \n \
- * Announce waiting times longer than one hour (e.g. waiting for the hosting provider) as "time for next update" \n \
- \n \
- * Update Ticket and Yammer AFTER incident is over, too. \n \
- \n \
-=== Recommended Checks and Actions === \n \
- * [wiki:NuetzlicheShell], [wiki:AdminHandbuch] \n \
- \n \
-=== Ask for Help (esp. during office hours) === \n \
- * Get help at big incidents and pair the incident \n \
- * Ask for help in communication/documentation for bigger incidents \n \
- \n \
-=== Post Mortem Analysis === \n \
- * How has it been resolved? What was the fix? \n \
- * How was the issue triggered? What have been the circumstances? \n \
- * What was affected? (see Incident, Who is already documented there) \n \
- * How can this situation be avoided in the future? (only relevant if "rule of three" triggered or effect is considered "major" by the team) \n \
- * Did a known standard solution work? If not: Document it! e.g. in [wiki:NuetzlicheShell], [wiki:AdminHandbuch] \n \
- * Close the ticket, if no further actions can be derived from it.'
+description_template = """=== Incident ===
+ * Does it affect only one user/colleague? Not an incident, normal support case.
+ * What has been noticed? (e.g. nagios check + host that failed)
+""" + comment_template + """
+ * Who is affected? (all users, limited set of users, departments, partners, ...)
+ * When did it start? (e.g. nagios reported time)
+ * How did you notice it (Monitoring, Support..?)
+
+=== Spread the word first, then act ===
+ * Always put a link to this ticket everywhere you inform your colleagues about it
+ * Tell Yammer group "System Status" when the incident started (latest 30min after incident notice!)
+ * Update Yammer at least every hour for incidents during office hours, even if nothing changed.
+ * Be available in Infrastruktur channel and communicate to your colleagues
+ * Announce waiting times longer than one hour (e.g. waiting for the hosting provider) as "time for next update"
+
+ * Update Ticket and Yammer AFTER incident is over, too.
+
+=== Recommended Checks and Actions ===
+ * [wiki:NuetzlicheShell], [wiki:AdminHandbuch]
+
+=== Ask for Help (esp. during office hours) ===
+ * Get help at big incidents and pair the incident
+ * Ask for help in communication/documentation for bigger incidents
+
+=== Post Mortem Analysis ===
+ * How has it been resolved? What was the fix?
+ * How was the issue triggered? What have been the circumstances?
+ * What was affected? (see Incident, Who is already documented there)
+ * How can this situation be avoided in the future? (only relevant if "rule of three" triggered or effect is considered "major" by the team)
+ * Did a known standard solution work? If not: Document it! e.g. in [wiki:NuetzlicheShell], [wiki:AdminHandbuch]
+ * Close the ticket, if no further actions can be derived from it.
+"""
 ######
 
 mail_notifications=True
